0e5fd981fd375834e69ebd0680ceb09555cef6d2
d3ciee <decefemz@gmail.com>
2024-04-25 13:12:30 +0200

    Change svelte config to use adapter node

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-CAFE MNANDI
+<script>
+	import { Button } from '$lib/ui/button';
+</script>
+
+<main class="flex h-screen w-full flex-col items-center justify-center gap-2">
+	WELCOME TO CW-INSIGHTS LOGIN: <Button href="/auth/sign-in">Login</Button>
+	<p>
+		<br /><br /><br />ADMIN LOGIN DETAILS: <br /><br /> EMAIL: admin@hit.ac.zw <br /> PASSWORD: 4dmin@Hit
+	</p>
+</main>
